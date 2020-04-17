29634c89ef9283cc27a318a072db9f889f4a9e3b
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-04-17 10:53:20 -0700

    simplifed components and app.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import StarWarsList from "./components/StarWarsList.js";
+import Characters from './components/CharacterCard';
+import Axios from 'axios';
+import styled from 'styled-components';
 
+
+const Wrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+`;
 
 
 const App = () => {
@@ -12,12 +21,33 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [character, setCharacter] = useState([]);
+
+  useEffect(() => {
+    Axios.get('https://swapi.co/api/people/')
+      .then(response => {
+        setCharacter(response.data.results);
+      })
+      .catch(error => console.log('Something is failing!', error))
+  }, [])
+
+
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <StarWarsList />
-    </div>
+      <Wrap>
+        {character.map(ch => 
+          <Characters 
+            name = {ch.name} 
+            birthyear = {ch['birth_year']}
+            gender = {ch['gender']}
+            hair_color = {ch['hair_color']}
+            height = {ch['height']} 
+          />
+        )}
+      </Wrap>
+      </div>
   );
 }
 
