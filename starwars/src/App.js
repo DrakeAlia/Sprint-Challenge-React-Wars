8521fc6afe351c273576724dfdec402d6dfdb85b
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-04-17 10:39:15 -0700

    added and change my components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Characters from './components/CharacterCard';
-import Axios from 'axios';
-import styled from 'styled-components';
+import StarWarsList from "./components/StarWarsList.js";
+import "../src/styles.css";
+
 
 
 const Wrap = styled.div`
@@ -20,30 +20,11 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-const [character, setCharacter] = useState([]);
-
-useEffect(() => {
-  Axios.get('https://swapi.co/api/people/')
-  .then(response => {
-    setCharacter(response.data.results);
-  })
-  .catch(error => console.log('Something is off!', error))
-}, [])
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <Wrap>
-        {character.map(ch => 
-          <Characters 
-            name = {ch.name} 
-            birthyear = {ch['birth_year']}
-            gender = {ch['gender']}
-            hair_color = {ch['hair_color']}
-            height = {ch['height']} 
-          />
-        )}
-      </Wrap>
+      <StarWarsList />
     </div>
   );
 }
